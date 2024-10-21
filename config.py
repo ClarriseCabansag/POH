f591f991c6b896d57adb59403059eeb543b80cfc
ClarriseCabansag <132436016+ClarriseCabansag@users.noreply.github.com>
2024-10-21 15:30:59 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = '0123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0802@Localhost:5432/POS'
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://postgres:0802@Localhost:5432/POS')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
